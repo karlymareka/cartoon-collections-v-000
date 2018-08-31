9a4bf35a0d51b2b9810d51d0d1c34e28f876d0bf
Karly Mareka <karly.mareka@gmail.com>
2018-08-31 18:31:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any?{|call|  
+  array.any?{|call| call.length > 4} 
 end
 
 def find_the_cheese# code an argument here
