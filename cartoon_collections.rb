2f5b81fdff187920350d81da60ac5a1c10edd4a9
Karly Mareka <karly.mareka@gmail.com>
2018-08-31 18:15:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  i = 0
+  while i < dwarf_names.length 
+    puts "#{i+1}. #{dwarf_names[i]}"
+    i += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  i = 0
+  new_array = []
+  while i < array.length 
+    new_array << "#{array[i].capitalize}!"
+  end
+  new_array
 end
 
 def long_planeteer_calls# code an argument here
